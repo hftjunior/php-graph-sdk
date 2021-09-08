49f2b4ca9dc1787f04caeab6e2a549d10d4835fd
Hilton Junior <hilton.terra@gmail.com>
2021-09-08 14:11:50 -0300

    Update Facebook.php

--- src/Facebook/Facebook.php
+++ src/Facebook/Facebook.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright 2017 Facebook, Inc.
- *
+ * teste
  * You are hereby granted a non-exclusive, worldwide, royalty-free license to
  * use, copy, modify, and distribute this software in source code or binary
  * form for use in connection with the web services and APIs provided by
